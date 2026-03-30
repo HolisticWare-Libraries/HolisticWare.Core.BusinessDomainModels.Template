6730b3f5dd555b019d91eb07adf94eaa1e916553
moljac <mcvjetko@holisticware.net>
2026-03-30 23:45:59 +0200

    MAUI Comet

---
+++ samples/clients/maui/App_MAUI_Comet/App_MAUI_Comet/Platforms/Android/MainApplication.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.Maui.Hosting;
+
+namespace App_MAUI_Comet;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)	: base(handle, ownership)
+	{
+	}
+	protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
+}
---
+++ samples/clients/maui/App_MAUI_Comet/App_MAUI_Comet/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui.Hosting;
+
+namespace App_MAUI_Comet;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
+}
+
