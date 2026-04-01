20396945c31587e994b7c2fb4dbb12ef8bd2b930
moljac <mcvjetko@holisticware.net>
2026-04-01 22:02:05 +0200

    MAUI mauiapp

---
+++ samples/clients/maui/App_MAUIApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace App_MAUIApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ samples/clients/maui/App_MAUIApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace App_MAUIApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
